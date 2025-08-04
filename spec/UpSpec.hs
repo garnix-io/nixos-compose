a8f4d5f013809478817a583dfa55aa4c3a31fcc0
Sönke Hahn <soenkehahn@gmail.com>
2025-08-04 16:23:00 -0400

    Move `up` tests into their own module (#33)

---
+++ spec/UpSpec.hs
@@ -0,0 +1,121 @@
+module UpSpec where
+
+import Context
+import Control.Concurrent (myThreadId, newEmptyMVar, putMVar, readMVar, threadDelay, throwTo)
+import Control.Exception (AsyncException (..))
+import Control.Monad (forever)
+import Cradle
+import Data.Text.IO qualified as T
+import Ki qualified
+import Net.IPv4 qualified as IPv4
+import State (VmState (..), readState, readVmState)
+import StdLib
+import System.IO qualified
+import Test.Hspec
+import TestUtils
+
+spec :: Spec
+spec = do
+  context "when no vm names are given" $ do
+    it "starts all vms" $ do
+      withMockContext ["a", "b", "c"] $ \ctx -> do
+        _ <- assertSuccess $ test ctx ["up"]
+        result <- assertSuccess $ test ctx ["status"]
+        result ^. #stdout `shouldBe` "a: running\nb: running\nc: running\n"
+
+    it "gives a nice message when no vms are defined" $ do
+      withMockContext [] $ \ctx -> do
+        result <- test ctx ["up"]
+        result `shouldBe` TestResult "" "No vms are defined. Nothing to do.\n" (ExitFailure 1)
+
+  describe "vm building state" $ do
+    context "when the nix build blocks" $ do
+      let blockingBuildVmScript :: Context -> Context
+          blockingBuildVmScript =
+            (#nixVms . #buildVmScript)
+              .~ ( \_ctx _vmName _ip -> do
+                     forever $ threadDelay 1_000_000
+                 )
+      it "locks the state of a vm when building the nixos config" $ do
+        withMockContext ["a"] $ \(blockingBuildVmScript -> ctx) -> do
+          Ki.scoped $ \scope -> do
+            _ <- Ki.fork scope $ do
+              test ctx ["up", "a"]
+            waitFor $ do
+              vmState <- readVmState ctx "a"
+              vmState `shouldBe` Starting {ip = IPv4.fromOctets 10 0 0 2}
+              test ctx ["up", "a"] `shouldReturn` TestResult "a: already starting\n" "" ExitSuccess
+              test ctx ["status", "a"] `shouldReturn` TestResult "a: starting\n" "" ExitSuccess
+              test ctx ["ip", "a"] `shouldReturn` TestResult "10.0.0.2\n" "" ExitSuccess
+
+      it "handles attempts to stop building vms gracefully" $ do
+        withMockContext ["a"] $ \(blockingBuildVmScript -> ctx) -> do
+          Ki.scoped $ \scope -> do
+            _ <- Ki.fork scope $ do
+              test ctx ["up", "a"]
+            waitFor $ do
+              test ctx ["down", "a"]
+                `shouldReturn` TestResult
+                  { stdout = "",
+                    stderr = "a: building, cannot stop a building vm\n",
+                    exitCode = ExitFailure 1
+                  }
+
+    it "locks the state of a vm when booting" $ do
+      let blockingRunVm :: Context -> Context
+          blockingRunVm =
+            (#nixVms . #runVm)
+              .~ ( \_ctx _verbosity _vmName _vmScript -> do
+                     forever $ threadDelay 1_000_000
+                 )
+      withMockContext ["a"] $ \(blockingRunVm -> ctx) -> do
+        Ki.scoped $ \scope -> do
+          _ <- Ki.fork scope $ do
+            test ctx ["up", "a"]
+          waitFor $ do
+            vmState <- readVmState ctx "a"
+            vmState `shouldBe` Starting {ip = IPv4.fromOctets 10 0 0 2}
+            test ctx ["up", "a"] `shouldReturn` TestResult "a: already starting\n" "" ExitSuccess
+            test ctx ["status", "a"] `shouldReturn` TestResult "a: starting\n" "" ExitSuccess
+            test ctx ["ip", "a"] `shouldReturn` TestResult "10.0.0.2\n" "" ExitSuccess
+
+  describe "when nix evaluation fails" $ do
+    let failingBuildVmScript :: Context -> Context
+        failingBuildVmScript =
+          #nixVms
+            . #buildVmScript
+            .~ ( \_ctx _vmName _ip -> do
+                   T.hPutStrLn System.IO.stderr "test output"
+                   throwIO $ ExitFailure 42
+               )
+    it "prints out the error message" $ do
+      withMockContext ["a"] $ \(failingBuildVmScript -> ctx) -> do
+        test ctx ["up", "a"] `shouldReturn` TestResult "" "Building NixOS config...\ntest output\n" (ExitFailure 42)
+
+    it "doesn't add a vm to the state" $ do
+      withMockContext ["a"] $ \(failingBuildVmScript -> ctx) -> do
+        test ctx ["up", "a"] `shouldReturn` TestResult "" "Building NixOS config...\ntest output\n" (ExitFailure 42)
+        (^. #vms) <$> readState ctx `shouldReturn` mempty
+        (^. #vde) <$> readState ctx `shouldReturn` Nothing
+
+    it "doesn't add a vm to the state when interrupted by an async exception, e.g. Ctrl-C" $ do
+      blockingOnBuild <- newEmptyMVar
+      let blockingBuildVmScript :: Context -> Context
+          blockingBuildVmScript =
+            (#nixVms . #buildVmScript)
+              .~ ( \_ctx _vmName _ip -> do
+                     putMVar blockingOnBuild ()
+                     forever $ threadDelay 1_000_000
+                 )
+      withMockContext ["a"] $ \(blockingBuildVmScript -> ctx) -> do
+        Ki.scoped $ \scope -> do
+          threadId <- newEmptyMVar
+          _ <- Ki.fork scope $ do
+            myThreadId >>= putMVar threadId
+            test ctx ["up", "a"]
+          readMVar blockingOnBuild
+          threadId <- readMVar threadId
+          throwTo threadId UserInterrupt
+          waitFor $ do
+            (^. #vms) <$> readState ctx `shouldReturn` mempty
+            (^. #vde) <$> readState ctx `shouldReturn` Nothing
